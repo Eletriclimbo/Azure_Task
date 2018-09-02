72e9e146060fde08b102f89f2f90dc337c4bb4c1
Eletriclimbo <32715736+Eletriclimbo@users.noreply.github.com>
2018-09-02 21:25:52 +0300

    Update Script_BackupScript_onEachVm.ps1

--- Scripts/Script_BackupScript_onEachVm.ps1
+++ Scripts/Script_BackupScript_onEachVm.ps1
@@ -1,5 +1,12 @@
-﻿  Get-AzureRmStorageAccount | select abconeoneone
- $storageContainer = Get-AzureRmStorageAccount | where {$_.StorageAccountName -eq 'abconeoneone'} | Get-AzureStorageContainer
- $FilePath = 'E:\1.html'
- $BlobName = 'MyFileSChedule.txt'
- $storageContainer | Set-AzureStorageBlobContent –File $FilePath –Blob $BlobName
+$acct = "abconeoneone"
+$key = "fP7FjAVL3LYclT8AGRybdHakPI0qOUHZQhfcypSzC/FGQpr3fP/gwHIy6LDU3WCofKLXnsrLgpPPLvE6F3Dknw=="
+$container = "azure-webjobs-hosts"
+$file = "C:\inetpub\wwwroot\index.htm"
+$properties = @{"ContentType" = "html"}
+$time = Get-Date -Format g
+$BlobName = "WebSite 2 $time"
+#create a context for communicating with azure storage
+$ctx = New-AzureStorageContext -StorageAccountName $acct -StorageAccountKey $key -Protocol Https
+
+#use Set-AzureStorageBlobContent to upload file
+Set-AzureStorageBlobContent -File $file -Container $container -Context $ctx -BlobType Block -Properties $properties  –Blob $BlobName
